f952ec982ee75e5adac7f4a16c97c005b085e4f6
Danben Erick <danbencb08@outlook.es>
2024-02-26 11:52:18 -0500

    Añadido cors

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI, HTTPException
 from starlette.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 import time
 from typing import Optional
 from reporte import generar_pdf_service
 app = FastAPI()
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
