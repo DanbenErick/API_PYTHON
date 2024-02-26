c06299e79620e789088300866f6150f920a6c660
Danben Erick <danbencb08@outlook.es>
2024-02-26 12:05:55 -0500

    CORS

--- main.py
+++ main.py
@@ -7,14 +7,9 @@ from reporte import generar_pdf_service
 app = FastAPI()
 
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
